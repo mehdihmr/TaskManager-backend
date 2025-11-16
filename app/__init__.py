0d528c55ae8040e3a076c70335faf2d21f0c747c
Mehdi Hamrouni <mehdi.hamrouni@itq.de>
2025-11-16 19:56:07 +0100

    configure JWT for the project

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
+from flask_jwt_extended import JWTManager
 from app.utilities import Logger
 
 db = SQLAlchemy()
 migrate = Migrate()
+jwt = JWTManager()
 
 
 def create_app():
@@ -18,11 +19,11 @@ def create_app():
     Logger.log_info(f"{__name__} - Starting the app...")
     app = Flask(__name__)
     CORS(app)
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
+    app.config.from_object("config")
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
     Logger.log_info(f"{__name__} - Database initialized.")
 
     from app.routes.task_routes import task_bp
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'tasks.db')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+JWT_SECRET_KEY = "e4cbd307-5096-4020-b7f0-b2d30d15f116"
