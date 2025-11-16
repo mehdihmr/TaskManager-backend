35433085482d6d519af07d8c56310ef92b1b815c
Mehdi Hamrouni <mehdi.hamrouni@itq.de>
2025-11-16 21:53:09 +0100

    jwt in .env

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'tasks.db')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-JWT_SECRET_KEY = "e4cbd307-5096-4020-b7f0-b2d30d15f116"
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
